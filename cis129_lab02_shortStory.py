0812b1b1482196de60b291b7fe868839821a0f67
Jcamacho34 <160698047+Jcamacho34@users.noreply.github.com>
2024-03-05 14:35:49 -0700

    Update cis129_lab02_shortStory.py
    extra lines added to the short story.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,15 @@
-print("hello, world")
+# Author: Jorge Camacho
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+print('There once was a boy living in a desolate town with a small population.')
+print('His favorite person was his uncle who showed him tons of cool things.')
+print('His father did not like this uncle, as he had a history of delinquency.')
+print('The boy and uncle went out to the beach to teach the boy how to swim.')
+print('The father did not like this news and ran to get the boy back.')
+print('The father catches up to the boy and uncle and begin to argue.')
+print('As they argue, the boy notices a bear cub, he gets near him and is immediately clawed at.')
+print('The uncle and father take notice and pull the boy back, as they make loud sounds to scare the bear cub away')
+print('The boy and uncle have a laugh as they make sure the boy is okay.')
+print('As they turn back to return home, a giant bear blasts through the bushes angry as can be.')
+print('The boy, uncle, and father all scream and zoom back to town.')
+print('All three return to their town.')
